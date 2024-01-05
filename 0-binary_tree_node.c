3385598e91c5c008eabfab3e7376aa72cb784f64
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-01-05 11:39:50 +0100

    task0 u

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_node - Creates a binary node
  * @parent: parent node
  * @value: node value
+ * 
  * Return: new node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
